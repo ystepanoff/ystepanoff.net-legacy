8c5596e1e8a03f80eda1804baa665e6b50ce9449
Yegor Stepanov <yegors@mustardsystems.com>
2023-01-12 15:03:40 +0000

    update skeleton

--- homepage/main/models.py
+++ homepage/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    identifier = models.TextField()
+    title = models.TextField()
+    image = models.TextField()
+    description = models.TextField()
+    url = models.TextField()
+    content = models.TextField()
+
--- homepage/main/views.py
+++ homepage/main/views.py
@@ -1,5 +1,25 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Project
 
 
 def index(request):
-    return render(request, "index.html", {})
+    projects = Project.objects.order_by('id')
+    paginator = Paginator(projects, 3)
+    for page in paginator:
+        print(list(page))
+    return render(
+        request,
+        "index.html",
+        {
+            'project_rows': [
+                [
+                    {
+                        'title': project.title,
+                        'description': project.description,
+                        'url': project.url,
+                    } for project in project_row
+                ] for project_row in paginator
+            ]
+        }
+    )
